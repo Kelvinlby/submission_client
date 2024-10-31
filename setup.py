cae5e824d23b6187fea38ee91f8bb2339b1ff04a
Patrick_Chen <patrickechohelloworld@outlook.com>
2024-10-31 14:09:48 +0800

    Pack halfway ...

--- setup.py
+++ setup.py
@@ -10,4 +10,8 @@ setup(
     description='A python package for kelvinlby/submission',
     url='https://github.com/ECHO-HELLO-WORLD424/submission_client',
     license='GNU GENERAL PUBLIC LICENSE Version 3',
+    install_requires=[
+        'grpcio',
+        'grpcio-tools',
+    ]
 )
