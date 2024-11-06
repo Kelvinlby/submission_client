ea3d55acb69d19a481c2509656db1e5183314b26
Patrick_Chen <patrickechohelloworld@outlook.com>
2024-11-06 10:00:44 +0800

    nothing

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import time
 
-from submission import  log_metric
+from submission import log_metric, start_job, end_job
+
 
 def main():
     for i in range (10):
+        start_job('train')
         log_metric('loss', 0.5+i)
         i += 1
+        end_job('train')
         time.sleep(5)
 
 if __name__ == "__main__":
