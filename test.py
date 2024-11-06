4f25f912f80e75b5d167006b4a261e34d054aadb
Patrick_Chen <patrickechohelloworld@outlook.com>
2024-11-06 14:19:28 +0800

    nothing

--- test.py
+++ test.py
@@ -6,9 +6,13 @@ def main():
     start_job('start_job')
     log_job('log_job', 0.75)
     log_metric('log_metric', 23)
+    time.sleep(5)
     log_metric('log_metric', 34)
+    time.sleep(5)
     log_metric('log_metric', 45)
+    time.sleep(5)
     log_metric('log_metric', 56)
+    time.sleep(5)
     log_metric('log_metric', 67)
 
 
