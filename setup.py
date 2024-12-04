d0f26d2b2603999983b4110a583758964b76db69
Patrick_Chen <patrickechohelloworld@outlook.com>
2024-12-04 19:22:01 +0800

    Release 1.2.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='submission',
-    version='1.1.1',
+    version='1.2.3',
     packages=find_packages(),
     package_data={
         'submission': ['*.proto']
